9af8ae4dfbc5297d169d034b673de100da33aa25
Camille Haus <146786066+CamilleHaus@users.noreply.github.com>
2026-01-16 15:15:31 -0300

    WIP

--- nivel-novato.c
+++ nivel-novato.c
@@ -7,6 +7,7 @@
 // - nome: até 29 caracteres + '\0'
 // - cor : até 9 caracteres + '\0'
 // - tropas: quantidade de tropas no territorio
+
 typedef struct
 {
     char nome[30];
@@ -14,6 +15,8 @@ typedef struct
     int tropas;
 } Territorio;
 
+
+// Função auxiliar que foi criada para limpar o restante da memória caso o usuário digite alem do tamanho permitido
 static void limpar_linha(void)
 {
     int c;
@@ -24,7 +27,7 @@ static void limpar_linha(void)
 
 int main(void)
 {
-    // Vetor (array) para armazenar 5 territorios
+    // Declaração de um array baseado na "tipagem" do struct
     Territorio territorios[TERRITORIOS_MAX];
 
     printf("=== Cadastro de Territorios (%d) ===\n\n", TERRITORIOS_MAX);
